44b621f642d02a846588f64b802aaaa3e8a23f0d
Rishabh Banerjee <32070588+Luke-Vader@users.noreply.github.com>
2023-01-17 10:51:33 -0800

    C# Hello World

---
+++ Introduction to Programming/FirstProject/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//System is a namespace existing in .NET FCL.
+
+using static System.Console;
+//This makes Console class functions accessible
+
+namespace FirstProject        
+//A Collection of classes(namespace) in this case FirstProject is user defined namespace.
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        //program execution will start from this method.
+        {
+            WriteLine("Hello World");
+            WriteLine("Douglas College \n \nBC");
+            //Console.WriteLine("BC");
+            WriteLine(4567);
+            WriteLine("****");
+            ReadKey();
+        }
+    }
+}
