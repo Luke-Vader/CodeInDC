173a08dc1bad77586f8fd452aeee051b0709fb8a
Rishabh Banerjee <rishabhbanerjee2000@gmail.com>
2023-01-18 18:56:28 -0800

    Question 3&6

---
+++ Introduction to Programming/Lab1/Lab1_PEQ3/Lab1_PEQ3/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace Lab1_PEQ3
+{
+    class Printer
+    {
+       static void Main()
+        {
+            Write("##\t\t\t\t\t\t\t##\n" +
+                  "\t##\t\t\t\t\t##\n" +
+                  "\t\t##\t\t\t##\n" +
+                  "\t\t\t##\t##\n" +
+                  "\t\t\t ## # ##\n" +
+                  "\t\t\t##\t##\n" +
+                   "\t\t##\t\t\t##\n" +
+                   "\t##\t\t\t\t\t##\n" +
+                   "##\t\t\t\t\t\t\t##\n");
+        }
+    }
+}
---
+++ Introduction to Programming/Lab1/Lab1_PEQ6/Lab1_PE6/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+namespace Lab1_PE6
+{
+    class Header
+    {
+        static void Main()
+        {
+            //Output 1
+            WriteLine("Output #1");
+            Write("\tYou either die a hero ");
+            Write("or live long enough ");
+            Write("to see yourself become the villain.\n");
+
+            //Output 2
+            WriteLine("\nOutput #2");
+            Write("\tYou either die a hero\n");
+            Write("\tor live long enough\n");
+            Write("\tto see yourself become the villain.\n");
+
+            //Output 3
+            WriteLine("\nOutput #3");
+            WriteLine("\tYou\n\teither\n\tdie\n\ta\n\thero\n" +
+                "\tor\n\tlive\n\tlong\n\tenough\n" +
+                "\tto\n\tsee\n\tyourself\n\tbecome\n\tthe\n\tvillain.\n");
+        }
+
+    }
+}
