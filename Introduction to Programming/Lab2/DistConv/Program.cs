773df1812c5d7325ec589df52abded03533b268c
Rishabh Banerjee <32070588+Luke-Vader@users.noreply.github.com>
2023-01-24 11:29:02 -0800

    Distance Converter

---
+++ Introduction to Programming/Lab2/DistConv/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace DistConv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float miles = 4.5f;
+            float feet = miles * 5280;
+            double km = miles * 1.609344;
+
+            WriteLine($"\t\tDistance Converter App\nMiles: {miles:f2}" +
+                $"\n\t\tEquivalent Values\nFeet: {feet:n0}" +
+                $"\nKilometres: {km:f2}");
+            WriteLine();
+        }
+    }
+}
---
+++ Introduction to Programming/Lab2/TempConv/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace TempConv
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //data declarations
+            int celsius;
+            float farenheit;
+
+            WriteLine("\t\tTemperature Conversion App\nEnter Temperature:");
+            ReadLine(celsius);
+
+        }
+    }
+}
