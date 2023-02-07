104f435720b602894b4dc90c1e90b7448f7fb23b
Rishabh Banerjee <32070588+Luke-Vader@users.noreply.github.com>
2023-02-07 12:14:57 -0800

    Lab4 Classwork

---
+++ Introduction to Programming/Lab4/MotorWayApp/MotorWayApp/Motorway.cs
@@ -0,0 +1,68 @@
+﻿using static System.Console;
+
+namespace MotorWayApp
+{
+    internal class Motorway
+    {
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Direction { get; set; }
+        public string Surface { get; set; }
+        public int Lanes { get; set; }
+        public string Toll { get; set; }
+        public string Maint { get; set; }
+
+        //Default Constructor
+        public Motorway() { }
+
+        //Constructor with Name, Type, Number of Lanes
+        public Motorway(string name, string type, int lanes)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Lanes = lanes;
+        }
+
+        //Full Constructor
+        public Motorway(string name, string type, string direction, string surface, int lanes, string toll, string maint)
+        {
+            Name = name;
+            Type = type;
+            Direction = direction;
+            Surface = surface;
+            Lanes = lanes;
+            Toll = toll;
+            Maint = maint;
+        }
+
+        public void DisplayName() {
+            WriteLine($"\nMotorway Name: {this.Name}");
+        }
+
+        public void DisplayNameToll()
+        {
+            WriteLine($"\nMotorway Name: {this.Name}" +
+                $"\nToll Status {this.Toll}");
+        }
+
+        public void DisplayNameLanes() 
+        {
+            WriteLine($"\nMotorway Name: {this.Name}" +
+                $"\nNumber of Lanes: {this.Lanes}");
+
+        }
+
+        public override string ToString()
+        {
+            return $"\t\tMotorway Details\n" +
+                $"\nName: {Name}" +
+                $"\nType: {Type}" +
+                $"\nDirection: {Direction}" +
+                $"\nSurface: {Surface}" +
+                $"\nNumber of Lanes: {Lanes}" +
+                $"\nToll: {Toll}" +
+                $"\nMaintainance Party: {Maint}";
+        }
+    }
+}
---
+++ Introduction to Programming/Lab4/MotorWayApp/MotorWayApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+
+namespace MotorWayApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //data declarations
+            string input;
+            Motorway mw = new Motorway();
+
+            //taking data from the user
+            Write("\t\tEnter Motorway Details\n" + "Name: ");
+            mw.Name = ReadLine();
+
+            Write("Type: ");
+            mw.Type = ReadLine();
+
+            Write("Direction: ");
+            mw.Direction = ReadLine();
+
+            Write("Surface: ");
+            mw.Surface = ReadLine();
+
+            Write("Number of Lanes: ");
+            mw.Lanes = int.Parse(ReadLine());
+
+            Write("Toll Status: ");
+            mw.Toll = ReadLine();
+
+            Write("Maintainence Party: ");
+            mw.Maint = ReadLine();
+            
+            //display methods 
+            mw.DisplayName();
+            mw.DisplayNameLanes();
+            mw.DisplayNameToll();
+
+            WriteLine(mw.ToString());
+            ReadKey();
+        }
+    }
+}
