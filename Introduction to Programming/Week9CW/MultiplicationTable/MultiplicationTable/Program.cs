bdfc8b62e245865940c578fac633ab5d4e74b27d
Rishabh Banerjee <32070588+Luke-Vader@users.noreply.github.com>
2023-03-07 09:40:30 -0800

    Bonus HW code??

---
+++ Introduction to Programming/Week9CW/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Remoting.Messaging;
+using static System.Console;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int n;
+            int ctr = 1;
+
+            Write("Enter a Number: ");
+            input = ReadLine();
+            n = int.Parse(input);
+
+            while(ctr < 11)
+            {
+                WriteLine($"{ctr} x {n} = {ctr*n}");
+                ctr++;
+            }
+
+        }
+    }
+}
