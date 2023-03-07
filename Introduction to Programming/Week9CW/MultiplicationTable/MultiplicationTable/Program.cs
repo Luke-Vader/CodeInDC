9b29e10ca7661087abd6a1511e47faa97cbbd3e0
Rishabh Banerjee <32070588+Luke-Vader@users.noreply.github.com>
2023-03-07 09:41:32 -0800

    Comments added

--- Introduction to Programming/Week9CW/MultiplicationTable/MultiplicationTable/Program.cs
+++ Introduction to Programming/Week9CW/MultiplicationTable/MultiplicationTable/Program.cs
@@ -7,14 +7,17 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
+            //data declarations
             string input;
             int n;
             int ctr = 1;
 
+            //user input and conversion
             Write("Enter a Number: ");
             input = ReadLine();
             n = int.Parse(input);
 
+            //loop to display and multiply values
             while(ctr < 11)
             {
                 WriteLine($"{ctr} x {n} = {ctr*n}");
