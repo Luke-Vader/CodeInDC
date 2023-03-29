236ad2b5f2f1a77ad38acc8aa1c283a7aa6ca5c5
Rishabh Banerjee <rishabhbanerjee2000@gmail.com>
2023-03-29 13:46:49 -0700

    Assignment done(without debug)

--- Introduction to Programming/Assignments/RishabhA2_1175_001/RishabhA2_1175_001/Book.cs
+++ Introduction to Programming/Assignments/RishabhA2_1175_001/RishabhA2_1175_001/Book.cs
@@ -3,18 +3,59 @@ namespace RishabhA2_1175_001
 {
 	public class Book
 	{
-		public Book()
-		{
-			int id;
-			string name;
-			string author;
-			string[] location;
-			int[] copies;
+        //fields
+        string id;
+        string name;
+        string author;
+        string[] locations;
+        int[] copies;
 
+        //paramtereised constructor
+        public Book(string id, string name, string author, string[] locations, int[] copies)
+        {
+            this.id = id;
+            this.name = name;
+            this.author = author;
+            this.locations = locations;
+            this.copies = copies;
+        }
 
+        //default constructor
+        public Book() { }
 
+        public string GetId() => this.id;
+        public string GetName() => this.name;
+        public string GetAuthor() => this.author;
+        public string[] GetLocations() => this.locations;
+        public int[] GetCopies() => this.copies;
 
-		}
-	}
+        public override string ToString()
+        {
+            //data declarations
+            string avail = "";
+            int max = this.copies[0];
+            int max_pos = 0;
+            int total = 0;
+            int ctr = 0;
+
+            //finding the maximum available copies location
+            foreach(string location in this.locations)
+            {
+                if (max < copies[ctr])
+                {
+                    max = copies[ctr];          //max quantity
+                    max_pos = ctr;              //max location position 
+                }
+                total += copies[ctr];
+                avail += "\n" + location + "\t\t" + this.copies[ctr];
+                ctr++;
+            }
+            return $"Book Id : {this.id} name : {this.name}" +
+                $"Location \t\t Number of Books Available" +
+                $"{avail}" +
+                $"\n\n{total} are the total number of copies" +
+                $"\n{this.locations[ctr]} has the maximum number of copies";
+        }
+    }
 }
 
---
+++ Introduction to Programming/Assignments/RishabhA2_1175_001/RishabhA2_1175_001/LibraryApp.cs
@@ -0,0 +1,138 @@
+﻿using static System.Console;
+
+namespace RishabhA2_1175_001
+{
+    internal class LibraryApp
+    {
+
+        static int index = 0;
+
+        static void Main()
+        {
+            //data declarations
+            Book[] books = new Book[30];
+
+            //function to display choices and execute relevant actions
+            switch (GetChoice())
+            {
+                case "A":
+                    AddBook(ref books);
+                    break;
+
+                case "S":
+                    SearchBook(ref books);
+                    break;
+
+                case "D":
+                    DisplayAll(ref books);
+                    break;
+
+                default:
+                    Write("Invalid Input");
+                    break;
+            }
+
+            ReadKey();
+        }
+
+        static string GetChoice()
+        {
+            Write("..................Library Database...................." +
+                "\n\nPlease select an option from the given menu:" +
+                "\n\n\tAdd a book to the database...........enter 'A'" +
+                "\n\tSearch a book in the database........enter 'S'" +
+                "\n\tDisplay all books in the database....enter 'D'" +
+                "\n\tExit.....enter 'E'" +
+                "\nEnter your choice here:");
+            return ReadLine();   
+        }
+
+        static void AddBook(ref Book[] books)
+        {
+
+            //staging variables
+            string id;
+            string name;
+            string author;
+            string[] locations = new string[4];
+            int[] copies = new int[4];
+
+            
+            Write("Enter book id: ");
+            id = ReadLine();
+            Write("Enter book name: ");
+            name = ReadLine();
+            Write("Enter author name: ");
+            author = ReadLine();
+            Write("Enter the location and number of copies for four locations");
+
+            for (int i = 0; i < locations.Length; ++i)
+            {
+                Write($"Enter location {i}: ");
+                locations[i] = ReadLine();
+                Write("Enter the number of copies: ");
+                copies[i] = int.Parse(ReadLine());
+            }
+
+            Book book = new Book(id, name, author, locations, copies);
+            if(index == 0)
+            {
+                books[0] = book;
+            } else
+            {
+                index++;
+                books[index] = book;
+            }
+
+        }
+
+        static void SearchBook(ref Book[] books)
+        {
+            string id = "";                         //id of book to search
+            Book searchedBook = new Book(); //staging the searched book
+            if(books.Length != 0)
+            {
+                Write("Enter Book ID to search: ");
+                id = ReadLine();
+                int index = 0;
+                    foreach(Book book in books)             //searching array against id
+                    {
+                        if(book.GetId() == id)
+                        {
+                            searchedBook = book;
+                        }
+                        index++;
+                    }                
+            } else
+            {
+                Write("The array is empty");
+            }
+
+            if (searchedBook.GetId() != null)
+            {
+                Write($"Book with {id} was not found");
+            }
+            else
+            {
+                Write(searchedBook.ToString());
+            }
+        }
+
+        static void DisplayAll(ref Book[] books)
+        {
+
+            if(books.Length == 0)
+            {
+                WriteLine("There is no data to display");
+            }
+            else
+            {
+                foreach(Book book in books)
+                {
+                    Write(book.ToString());
+                }
+            }
+
+        }
+    }
+}
