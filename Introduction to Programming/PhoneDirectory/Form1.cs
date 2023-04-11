0402cf928c3a07ba5ed76a96a5ca255493a5f9b0
Rishabh Banerjee <rishabhbanerjee2000@gmail.com>
2023-04-11 11:54:02 -0700

    Phone Directory

---
+++ Introduction to Programming/PhoneDirectory/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PhoneDirectory
+{
+    public partial class PhoneDirectory : Form
+    {
+        public PhoneDirectory()
+        {
+            InitializeComponent();
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            if (txtBxPhone.Text.ToString().Length != 10)
+                lblError.Visible = true;
+            else
+            {
+                lblError.Visible = false;
+                try
+                {
+                    StreamWriter sw = new StreamWriter("PhoneRecords.txt");
+                    sw.WriteLine(txtBxName.Text + "|" + txtBxAdd.Text + "|*" + txtBxPhone.Text + "*");
+                    sw.Close();
+                    MessageBox.Show("Record Saved Successfully");
+                }
+                catch (IOException exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+
+        }
+    }
+}
