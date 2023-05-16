3d829f62bddc095dc838d4441db54553c10b68b1
Rishabh Banerjee <rishabhbanerjee2000@gmail.com>
2023-05-15 22:09:09 -0700

    Homework 1 finished

---
+++ Multimedia Web Development/Homework_Ex1/solution/appList.js
@@ -0,0 +1,67 @@
+const apps = [
+	{
+		name: "WhatsApp",
+	},
+	{
+		name: "Facebook",
+	},
+	{
+		name: "Facebook Messenger",
+	},
+	{
+		name: "Instagram",
+	},
+	{
+		name: "Snapchat",
+	},
+	{
+		name: "UC Browser",
+	},
+	{
+		name: "Uber",
+	},
+	{
+		name: "Youtube",
+	},
+	{
+		name: "SHAREit",
+	},
+	{
+		name: "Bitmoji",
+	},
+	{
+		name: "Google Search",
+	},
+	{
+		name: "Google Play",
+	},
+	{
+		name: "Google Maps",
+	},
+	{
+		name: "Gmail",
+	},
+	{
+		name: "iCloud",
+	},
+	{
+		name: "Amazon Mobile",
+	},
+	{
+		name: "Twitter",
+	},
+	{
+		name: "Netflix App",
+	},
+	{
+		name: "Google+",
+	},
+	{
+		name: "Pinterest",
+	},
+	{
+		name: "Google Pay(Tez)",
+	},
+];
+
+export default apps;
