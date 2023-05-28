be67f83eb28577969590ea49a6b197d724bdcdde
Rishabh Banerjee <rishabhbanerjee2000@gmail.com>
2023-05-28 00:03:46 -0700

    Assignment 1 done, finally with comments

--- Advanced Java/TwentyOne/src/Game.java
+++ Advanced Java/TwentyOne/src/Game.java
@@ -5,29 +5,28 @@ public class Game {
     public int userSum;
     public int cpuSum;
 
-    Game(){
-        Random rand = new Random();
-        cpuSum = rand.nextInt(22-1) + 1;
+    Game() {
+        cpuSum = new Random().nextInt(22 - 1) + 1;
         userSum = 0;
     }
 
-    public void playMove(){
-        Random random = new Random();
-        int card = random.nextInt(12 - 1) + 1;
+    public void playMove() {
+        int card = new Random().nextInt(12 - 1) + 1;            //drawing a new card
         System.out.println("The card is " + card);
         this.userSum += card;
 
+        //checking if the total increases 21 and game needs to be finished
         if (this.userSum > 21) {
-            determineWinner(1);
+            determineWinner(1);             //finish game because total exceeded 21
         } else {
             System.out.println("Your total is " + this.userSum);
         }
-
     }
 
-    public void determineWinner(int flag){
+    public void determineWinner(int flag) {
 
-        if(flag == 1) {
+        //flagged condition for finishing game
+        if (flag == 1) {
             System.out.println("Your total is " + this.userSum +
                     "\nThe computer's score is " + this.cpuSum +
                     "\nYou Lose");
--- Advanced Java/TwentyOne/src/Main.java
+++ Advanced Java/TwentyOne/src/Main.java
@@ -1,15 +1,26 @@
+//Author: Rishabh Banerjee
+//Date: 05/27/2023
+//Blackjack
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //data declarations
         Scanner scan = new Scanner(System.in);
         String contFlag = "y";
 
-        while(contFlag.equals("y") || contFlag.equals("Y")){
+        //loop to keep the game running
+        while (contFlag.equals("y") || contFlag.equals("Y")) {
+
+            //game object initialised with custom constructor
             Game beginGame = new Game();
             String choice;
+
+            //loop to keep drawing more cards for the user
             do {
-                beginGame.playMove();
+                beginGame.playMove();                           //function to draw card and check total score
                 if (beginGame.cpuSum != 0) {
                     System.out.print("Do you want another card (y/n)?");
                     choice = scan.next();
@@ -19,17 +30,22 @@ public class Main {
 
             } while (choice.equals("y") || choice.equals("Y"));
 
+            //checking if game was finished legally
             if (beginGame.cpuSum != 0)
                 beginGame.determineWinner(0);
 
             System.out.print("\n\nDo you wish to play again(y/n)?");
             contFlag = scan.next();
-        };
+        }
+        ;
 
+        //function to thank user for playing the game with ASCII art
         thankUser();
     }
 
     public static void thankUser() {
+
+        //Logic for displaying the Thank Triangle
         for (int i = 0; i < 6; ++i) {
             System.out.println("Thank you for playing");
             for (int j = 0; j <= i; ++j)
