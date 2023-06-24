c587bd8d3a5946212cb91abee066ec0e4a68c83d
Rishabh Banerjee <rishabhbanerjee2000@gmail.com>
2023-06-23 22:21:05 -0700

    Finished assignment 2 for advanced java with validations

--- Advanced Java/PlayerDataRishabh/src/Athlete.java
+++ Advanced Java/PlayerDataRishabh/src/Athlete.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Athlete {
 
     String firstName;
@@ -5,19 +7,33 @@ public class Athlete {
     int id;
     int[] points;
 
-    public Athlete(String firstName, String lastName, int id) {
+    public Athlete(String firstName, String lastName, int id, ArrayList<Integer> scores) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
+        this.points = new int[scores.size()];
+        for(int i = 0; i < scores.size(); ++i){
+            this.points[i] = scores.get(i);
+        }
     }
 
     public Athlete() {}
 
+    //method to calculate average score
     public double getAverageScore(){
         int sum = 0;
         for (int point: points)
             sum += point;
         return (double) sum /points.length;
     }
+
+    //method to display the
+    public void display(){
+        System.out.println(this.firstName + "\t"
+                        + this.lastName + "\t\t"
+                        + String.format("%.2f",this.getAverageScore()));
+
+    }
+
 
 }
--- Advanced Java/PlayerDataRishabh/src/Main.java
+++ Advanced Java/PlayerDataRishabh/src/Main.java
@@ -3,28 +3,81 @@ import java.util.Scanner;
 
 public class Main {
 
+    //data declarations
     static Scanner scan = new Scanner(System.in);
-    static ArrayList<Integer> scores = new ArrayList<>();
+    static ArrayList<Integer> scores;
+    static Athlete[] athletes;
 
     public static void main(String[] args) {
         int playerCount;
-        int scoreSentinel = 0;
+        String input;
 
         //determining the size of the Athlete array
-        System.out.println("How many players are you going to enter? ");
-        playerCount = scan.nextInt();
+        System.out.print("How many players are you going to enter? ");
+        input = scan.next();
+        playerCount = checkIfNumber(input);
 
         //declaring the space for the Athlete object data
         if (playerCount > 0) {
-            Athlete athletes[] = new Athlete[playerCount];
+            athletes = new Athlete[playerCount];
         }
 
-        System.out.println("Enter the details of Player 1: ");
+        //fetching the data from the user for the athlete data and score data
+        for (int i = 0; i < playerCount; ++i) {
 
-        while(scoreSentinel != -1){
-            scoreSentinel = scan.nextInt();
+            String firstName;
+            String lastName;
+            int id;
+            int choice =  0;
+            scores = new ArrayList<>();
+
+            System.out.print("Enter the first name ");
+            firstName = scan.next();
+
+            System.out.print("Enter the last name ");
+            lastName = scan.next();
+
+            System.out.print("Enter the id number ");
+            input = scan.next();
+            id = checkIfNumber(input);
+
+            //score data is being fetched
+            while(choice != -1){
+                System.out.print("Now enter the scores of this player, press -1 to end ");
+                input = scan.next();
+                choice = checkIfNumber(input);
+
+                if (choice != -1)
+                    scores.add(choice);
+            }
+
+            //creating the athlete object
+            Athlete athlete = new Athlete(firstName, lastName, id, scores);
+            athletes[i] = athlete;
+
         }
 
+        //display section for the score and the athlete name list
+        System.out.println("Players\t\t\t\tStats\n" +
+                "******************************");
+        for (Athlete athlete:
+             athletes) {
+            athlete.display();
+        }
 
+    }
+
+    //validation method to check for number
+    static int checkIfNumber(String s){
+        int number = -1;
+        try{
+            number = Integer.parseInt(s);
+        } catch (NumberFormatException e){
+            System.out.println("Not a Number");
+        }
+        if (number != -1)
+            return number;
+        else
+            return 0;
     }
 }
