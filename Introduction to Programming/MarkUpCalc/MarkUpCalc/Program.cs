26974b8379d82235527aabf360ffb7f56cebdb5a
Rishabh Banerjee <rishabhbanerjee2000@gmail.com>
2023-06-27 14:23:14 -0700

    finished the class assignment I gave

---
+++ Introduction to Programming/MarkUpCalc/MarkUpCalc/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace MarkUpCalc
+{
+    class Program
+    {
+        static void Main()
+        {
+            double wsPrice;
+            Write("Enter Wholesale Price: ");
+            wsPrice = Double.Parse(ReadLine());
+
+            WriteLine($"Wholesale Price($) \tMarkup(%) \tMarkup Price($)");
+            for(int i = 5; i <= 10; ++i)
+                WriteLine($"{wsPrice}\t\t\t{i}\t\t{wsPrice + (wsPrice * i) / 100}");
+            
+        }
+    }
+}
